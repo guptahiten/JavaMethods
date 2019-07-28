196bdabcfaa2be610a06c09f695169612b3736c6
Hiten <hk25gupta@gmail.com>
2019-07-28 20:59:43 +0530

    Initial commit

---
+++ src/com/hitengupta/Main.java
@@ -0,0 +1,20 @@
+package com.hitengupta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int highScore = calculateScore(true,800,8,1000);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true,10000,8,2000);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
+        }   return -1;
+    }
+}
